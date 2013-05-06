77da72af248f17322d57c7850e0787d2bf4c0904
Prakriti <c.prakriti@gmail.com>
2013-05-06 00:35:49 -0700

    commit

--- roc.cpp
+++ roc.cpp
@@ -1,38 +1,42 @@
 #include "roc.h"
 
+// ROC = Receiver Operating Characteristic of the Classifier
+
 ROC::ROC(const Mat& testlabels, Mat& responses, float truelabel)
     : testlabels(testlabels), responses(responses)
 {
-    Mat trueerror, false_error;
-    compare(testlabels, responses, trueerror, CMP_EQ);    false_error = (255 - trueerror);
-    int num_true = countNonZero(trueerror);               int num_false = testlabels.rows - num_true;
+    Mat correct, incorrect;
+    compare(testlabels, responses, correct, CMP_EQ);  // correct = 255 when (testlabels = responses), 0 otherwise
+    incorrect = (255 - correct);                      // incorrect = (max_value = 255) - correct
+    int num_correct = countNonZero(correct);
+    int num_incorrect = testlabels.rows - num_correct;
 
-    Mat true_locs, false_locs;
-    cv::findNonZero(trueerror, true_locs);
-    cv::findNonZero(false_error, false_locs);
+    Mat correct_locs, incorrect_locs;
+    cv::findNonZero(correct, correct_locs);
+    cv::findNonZero(incorrect, incorrect_locs);
     // Now isolate the true positives and true negatives
     TP = 0;
-    for (int i = 0; i < num_true; i++)
+    for (int i = 0; i < num_correct; i++)
     {
-        int x = true_locs.at<int>(i,1); // index of true response
+        int x = correct_locs.at<int>(i,1); // index of true response
         if (testlabels.at<float>(x,0) == truelabel)
         {
             TP++;
         }
     }
-    TN = num_true - TP;
+    TN = num_correct - TP;
 
     // Isolate the false positives and false negatives
     FP = 0;
-    for (int i = 0; i < num_false; i++)
+    for (int i = 0; i < num_incorrect; i++)
     {
-        int x = false_locs.at<int>(i,1); // index of false response
+        int x = incorrect_locs.at<int>(i,1); // index of false response
         if (testlabels.at<float>(x,0) != truelabel)
         {
             FP++;
         }
     }
-    FN = num_false - FP;
+    FN = num_incorrect - FP;
 }
 
 // Public function
@@ -52,13 +56,3 @@ float ROC::FScore()
 {
     return (2 * precision() * recall()) / (precision() + recall());
 }
-
-
-
-
-//ROC Curve : Receiver Operating Characteristic Curve :  Plot of True Positive Rate vs. False Positive Rate
-//    Ideally, knee of the curve should be as close to the top-left as possible (true positives)
-//void ROC::roc_curve()
-//{
-
-//}
